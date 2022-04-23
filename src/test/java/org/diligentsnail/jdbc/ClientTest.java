d9e9c964c10e609b424cfa2552e5ac8f750669f9
Denis Zavedeev <happy96538@gmail.com>
2022-04-23 22:50:16 +0300

    Fix mockito test

--- src/test/java/org/diligentsnail/jdbc/ClientTest.java
+++ src/test/java/org/diligentsnail/jdbc/ClientTest.java
@@ -1,6 +1,7 @@
 package org.diligentsnail.jdbc;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,24 +9,31 @@ import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 class ClientTest {
-	/*
-		@Test
-		void whenUserEnters1WeCallGetInboxByUsername() {
-			MailBoxClient mailBoxClient = Mockito.mock(MailBoxClient.class);
-			given(mailBoxClient.getInboxByUsername("abc"))
-					.willReturn(List.of());
+	@Test
+	void whenUserEnters1WeCallGetInboxByUsernameMockito() {
+		// Создаём мок MailBoxClient'а
+		MailBoxClient mailBoxClient = Mockito.mock(MailBoxClient.class);
 
-			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-			Client client = new Client(
-					mailBoxClient, new Scanner("abc\n1"), new PrintStream(byteArrayOutputStream)
-			);
-			client.run();
+		// 1. Когда вызовут метод getInboxByUsername c аргументом "abc" вернуть пустой список
+		given(mailBoxClient.getInboxByUsername("abc"))
+				.willReturn(List.of());
 
-			then(mailBoxClient).should().getInboxByUsername("abc");
-		}
-	*/
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		Client client = new Client(
+				mailBoxClient,
+				new Scanner("abc\n1"),
+				new PrintStream(byteArrayOutputStream)
+		);
+		client.run();
+
+		// Проверяем, что действительно вызвали метод с аргументом "abc"
+		then(mailBoxClient).should().getInboxByUsername("abc");
+	}
+
 	@Test
 	void whenUserEnters1WeCallGetInboxByUsername() {
 		TestMailboxClient testMailboxClient = new TestMailboxClient();
